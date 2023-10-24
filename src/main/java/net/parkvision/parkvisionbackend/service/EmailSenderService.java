a34732f107f9b10d654c2527ba0ed004c6b03e75
Filipstrozik <filipshelby@gmail.com>
2023-10-24 21:32:21 +0200

    first reservation email template

--- src/main/java/net/parkvision/parkvisionbackend/service/EmailSenderService.java
+++ src/main/java/net/parkvision/parkvisionbackend/service/EmailSenderService.java
@@ -2,6 +2,9 @@ package net.parkvision.parkvisionbackend.service;
 
 import java.io.File;
 
+import lombok.RequiredArgsConstructor;
+import net.parkvision.parkvisionbackend.model.Parking;
+import net.parkvision.parkvisionbackend.model.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
@@ -10,12 +13,21 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import jakarta.mail.internet.MimeMessage;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
+@RequiredArgsConstructor
 public class EmailSenderService {
+
+    public static final String EMAIL_TEMPLATE = "emailtemplate";
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    @Autowired
+    private final TemplateEngine templateEngine;
+
 
     public void sendEmail(String to, String body, String topic) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -47,4 +59,74 @@ public class EmailSenderService {
         javaMailSender.send(message);
         System.out.println("Email sent to " + to);
     }
+
+    public void sendHtmlEmail(
+            String firstName,
+            String lastName,
+            String to,
+            Parking parking,
+            Reservation reservation,
+            String topic) throws Exception {
+        Context context = new Context();
+        context.setVariable("title", "Potwierdzenie rezerwacji");
+        context.setVariable("description", "Oto potwierdzenie rezerwacji jaką dokonałeś w naszym systemie:");
+        context.setVariable("name", firstName + " " + lastName);
+        String htmlTable = generateHTMLTable(reservation, parking);
+        context.setVariable("body", htmlTable);
+
+        String text = templateEngine.process(EMAIL_TEMPLATE, context);
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom("${spring.mail.username}");
+        helper.setTo(to);
+        helper.setSubject(topic);
+        helper.setText(text, true);
+
+        javaMailSender.send(message);
+        System.out.println("Email sent to " + to);
+    }
+
+    public String generateHTMLTable(Reservation reservation, Parking parking) {
+        StringBuilder htmlTable = new StringBuilder();
+        htmlTable.append("<table style=\"width: 100%\">");
+
+        // Create rows for Reservation fields
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Numer rezerwacji</th>");
+        htmlTable.append("<td>").append(reservation.getId()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Data rozpoczęcia</th>");
+        htmlTable.append("<td>").append(reservation.getStartDate()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Data zakończenia</th>");
+        htmlTable.append("<td>").append(reservation.getEndDate()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Numer rejestracyjny</th>");
+        htmlTable.append("<td>").append(reservation.getRegistrationNumber()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Parking</th>");
+        htmlTable.append("<td>").append(parking.getName()).append(", ").append(parking.getStreet()).append(", ").append(parking.getCity()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Numer miejsca postojowego</th>");
+        htmlTable.append("<td>").append(reservation.getParkingSpot().getId()).append("</td>");
+        htmlTable.append("</tr>");
+
+
+        htmlTable.append("</table>");
+
+        return htmlTable.toString();
+    }
+
 }
