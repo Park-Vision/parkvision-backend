aa4e51aaeaafe5605bd504567fb47e1a24469207
Filip Strózik <47420153+Filipstrozik@users.noreply.github.com>
2023-10-25 19:19:28 +0200

    KAN-74 chcialbym dostac maila ze szczegołami rezerwacji

---
+++ src/main/java/net/parkvision/parkvisionbackend/config/AsyncConfig.java
@@ -0,0 +1,18 @@
+package net.parkvision.parkvisionbackend.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "emailTaskExecutor")
+    public TaskExecutor emailTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // Number of threads in the pool
+        executor.setMaxPoolSize(10); // Maximum number of threads in the pool
+        executor.setQueueCapacity(25); // Maximum queue size
+        return executor;
+    }
+}
--- src/main/java/net/parkvision/parkvisionbackend/service/EmailSenderService.java
+++ src/main/java/net/parkvision/parkvisionbackend/service/EmailSenderService.java
@@ -2,20 +2,33 @@ package net.parkvision.parkvisionbackend.service;
 
 import java.io.File;
 
+import lombok.RequiredArgsConstructor;
+import net.parkvision.parkvisionbackend.model.Parking;
+import net.parkvision.parkvisionbackend.model.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import jakarta.mail.internet.MimeMessage;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
+@RequiredArgsConstructor
 public class EmailSenderService {
+
+    public static final String EMAIL_TEMPLATE = "emailtemplate";
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    @Autowired
+    private final TemplateEngine templateEngine;
+
 
     public void sendEmail(String to, String body, String topic) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -47,4 +60,75 @@ public class EmailSenderService {
         javaMailSender.send(message);
         System.out.println("Email sent to " + to);
     }
+
+    @Async("emailTaskExecutor")
+    public void sendHtmlEmailReservationCreated(
+            String firstName,
+            String lastName,
+            String to,
+            Parking parking,
+            Reservation reservation,
+            String topic) throws Exception {
+        Context context = new Context();
+        context.setVariable("title", "Reservation confirmation");
+        context.setVariable("description", "Here is the confirmation of the reservation you made in our system:");
+        context.setVariable("name", firstName + " " + lastName);
+        String htmlTable = generateHTMLTable(reservation, parking);
+        context.setVariable("body", htmlTable);
+
+        String text = templateEngine.process(EMAIL_TEMPLATE, context);
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom("${spring.mail.username}");
+        helper.setTo(to);
+        helper.setSubject(topic);
+        helper.setText(text, true);
+
+        javaMailSender.send(message);
+        System.out.println("Email sent to " + to);
+    }
+
+    public String generateHTMLTable(Reservation reservation, Parking parking) {
+        StringBuilder htmlTable = new StringBuilder();
+        htmlTable.append("<table style=\"width: 100%\">");
+
+        // Create rows for Reservation fields
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Reservation number</th>");
+        htmlTable.append("<td>").append(reservation.getId()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Start date</th>");
+        htmlTable.append("<td>").append(reservation.getStartDate()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>End date</th>");
+        htmlTable.append("<td>").append(reservation.getEndDate()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Registration number</th>");
+        htmlTable.append("<td>").append(reservation.getRegistrationNumber()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Parking</th>");
+        htmlTable.append("<td>").append(parking.getName()).append(", ").append(parking.getStreet()).append(", ").append(parking.getCity()).append("</td>");
+        htmlTable.append("</tr>");
+
+        htmlTable.append("<tr>");
+        htmlTable.append("<th>Parking spot number</th>");
+        htmlTable.append("<td>").append(reservation.getParkingSpot().getId()).append("</td>");
+        htmlTable.append("</tr>");
+
+
+        htmlTable.append("</table>");
+
+        return htmlTable.toString();
+    }
+
 }
