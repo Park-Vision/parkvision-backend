676eb88b185e9274045729e8c68a05ae37f06dea
Filip Strózik <47420153+Filipstrozik@users.noreply.github.com>
2023-11-16 22:13:27 +0100

    OTHER fix offsetDateTime (#47)

--- src/main/java/net/parkvision/parkvisionbackend/model/Reservation.java
+++ src/main/java/net/parkvision/parkvisionbackend/model/Reservation.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.TimeZoneStorage;
+import org.hibernate.annotations.TimeZoneStorageType;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
@@ -19,8 +21,10 @@ public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String startDate;
-    private String endDate;
+    @TimeZoneStorage(TimeZoneStorageType.COLUMN)
+    private OffsetDateTime startDate;
+    @TimeZoneStorage(TimeZoneStorageType.COLUMN)
+    private OffsetDateTime endDate;
     private String registrationNumber;
     private double amount;
 
@@ -30,36 +34,4 @@ public class Reservation {
     private ParkingSpot parkingSpot;
     @OneToMany
     private List<Payment> payment;
-
-
-    public OffsetDateTime getStartDate() {
-        if (startDate != null) {
-            return OffsetDateTime.parse(startDate, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        }
-        return null; // Handle null values if needed
-    }
-
-    public void setStartDate(OffsetDateTime startDate) {
-        if (startDate != null) {
-            this.startDate = startDate.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        } else {
-            this.startDate = null; // Handle null values if needed
-        }
-    }
-
-    // Getter and setter methods for endDate
-    public OffsetDateTime getEndDate() {
-        if (endDate != null) {
-            return OffsetDateTime.parse(endDate, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        }
-        return null; // Handle null values if needed
-    }
-
-    public void setEndDate(OffsetDateTime endDate) {
-        if (endDate != null) {
-            this.endDate = endDate.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        } else {
-            this.endDate = null; // Handle null values if needed
-        }
-    }
 }
