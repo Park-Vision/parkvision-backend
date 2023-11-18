11623a95248eec13d119dfa8ea38297fb25bf07b
Szymon Romanek <srszymonsr@gmail.com>
2023-11-18 18:29:42 +0100

    Logging for nonexistent drone

--- src/main/java/net/parkvision/parkvisionbackend/kafka/KafkaListeners.java
+++ src/main/java/net/parkvision/parkvisionbackend/kafka/KafkaListeners.java
@@ -39,32 +39,9 @@ public class KafkaListeners {
         Optional<Drone> drone = droneService.getDroneById(Long.valueOf(droneId));
         System.out.println("Received Message from Drone " + droneId + ": " + message);
 
-        try {
-            Map result = new ObjectMapper().readValue(message, HashMap.class);
-            if (result.get("command").equals("start")) {
-                DroneMission droneMission = new DroneMission();
-                droneMission.setDrone(drone.get());
-                droneMission.setParking(drone.get().getParking());
-                droneMission.setMissionStartDate(OffsetDateTime.now().minusHours(1));
-                droneMission.setMissionEndDate(OffsetDateTime.now());
-                droneMission.setStatus("DONE");
-                List<MissionSpotResult> missionSpotResultList = new ArrayList<>();
-                MissionSpotResult missionSpotResult = new MissionSpotResult();
-                missionSpotResult.setParkingSpot(parkingSpotService.getParkingSpotById(1L).get());
-                missionSpotResult.setOccupied(true);
-                missionSpotResultList.add(missionSpotResult);
-                MissionSpotResult missionSpotResult2 = new MissionSpotResult();
-                missionSpotResult2.setParkingSpot(parkingSpotService.getParkingSpotById(2L).get());
-                missionSpotResult2.setOccupied(false);
-                missionSpotResultList.add(missionSpotResult2);
-
-                droneMission.setMissionSpotResultList(missionSpotResultList);
-                droneMissionService.createDroneMission(droneMission);
-            }
-
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        drone.ifPresent(value -> template.convertAndSend("/topic/parkings/" + value.getParking().getId(), message));
+        drone.ifPresentOrElse(
+            value -> template.convertAndSend("/topic/drones/" + droneId, message),
+            () -> System.out.println("Drone not existent, skipping websocket send")
+        );
     }
 }
