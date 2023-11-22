15d5466ec8640575fbdef0e370124f05559d0b3a
Filipstrozik <filipshelby@gmail.com>
2023-11-22 18:19:00 +0100

    fix order of creating parking

--- src/main/java/net/parkvision/parkvisionbackend/controller/ParkingController.java
+++ src/main/java/net/parkvision/parkvisionbackend/controller/ParkingController.java
@@ -10,12 +10,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
